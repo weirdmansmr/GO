4c2f7414f1280deb341f18760839de778aba7741
WeirdmanSMR <maga.saykhanov95@gmail.com>
2024-02-03 21:19:12 +0300

    new trial

--- main.go
+++ main.go
@@ -2,32 +2,44 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	printResults()
+	totalPriceChecker()
 }
 
-func printResults() {
-	fmt.Printf("Circle's square: %f sm^2 \n", calculateCircleArea(5))
-	fmt.Printf("Rect square: %d \n", calculateRectangularSquare(5, 5))
-	fmt.Printf("Rect permeter: %d \n", calculateRectangularPerimeter(5, 5))
-	fmt.Printf("Triangle square: %f \n", calculateTriangleSquare(5, 3))
+func totalPriceChecker()  {
+	price := 100
+	fmt.Println("Price is", price, "dollars.")
+	taxRate := 0.08
+	tax := float64(price) * taxRate
+	fmt.Println("Tax is", tax, "dollars.")
+	total := float64(price) + tax
+	fmt.Println("Total cost is", total, "dollars.")
+	availableFunds := 120
+	fmt.Println(availableFunds, "dollars available.")
+	fmt.Println("Within budget?", total <= float64(availableFunds))
 }
 
-func calculateCircleArea(r int) float32 {
-	return float32(r) * float32(r) * math.Pi
-}
+// func printResults() {
+// 	fmt.Printf("Circle's square: %f sm^2 \n", calculateCircleArea(5))
+// 	fmt.Printf("Rect square: %d \n", calculateRectangularSquare(5, 5))
+// 	fmt.Printf("Rect permeter: %d \n", calculateRectangularPerimeter(5, 5))
+// 	fmt.Printf("Triangle square: %f \n", calculateTriangleSquare(5, 3))
+// }
 
-func calculateRectangularSquare(a int, b int) int {
-	return a * b
-}
+// func calculateCircleArea(r int) float32 {
+// 	return float32(r) * float32(r) * math.Pi
+// }
 
-func calculateRectangularPerimeter(a int, b int) int {
-	return a*2 + b*2
-}
+// func calculateRectangularSquare(a int, b int) int {
+// 	return a * b
+// }
 
-func calculateTriangleSquare(b int, h int) float32 {
-	return float32(h) * float32(b) / 2
-}
+// func calculateRectangularPerimeter(a int, b int) int {
+// 	return a*2 + b*2
+// }
+
+// func calculateTriangleSquare(b int, h int) float32 {
+// 	return float32(h) * float32(b) / 2
+// }
